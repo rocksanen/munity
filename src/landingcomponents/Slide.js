03aff9cac427cc885277bd65630300539635a459
Otto Oksanen <rocksanen@hotmail.com>
2024-12-31 23:56:09 +0200

    second

--- src/landingcomponents/Slide.js
+++ src/landingcomponents/Slide.js
@@ -8,8 +8,6 @@ import SlideLBtn2 from "../images/SlideLBtn2.svg";
 import SlideRBtn2 from "../images/SlideRBtn2.svg";
 import SlideLBtnHo2 from "../images/SlideLBtnHo2.svg";
 import SlideRBtnHo2 from "../images/SlideRBtnHo2.svg";
-import Minihorse1 from "../images/Minihorse1.png";
-import Minihorse2 from "../images/Minihorse2.png";
 import fix2 from "../images/fix2.png";
 import fix3 from "../images/fix3.png";
 import fix4 from "../images/fix4.png";
@@ -17,11 +15,14 @@ import fix from "../images/fotor.png";
 
 const images = [Slide1, Slide2, Slide3];
 const delay = 4500;
+
 export const Slide = () => {
   const [index, setIndex] = useState(0);
   const [isHoveredL, setIsHoveredL] = useState(false);
   const [isHoveredR, setIsHoveredR] = useState(false);
+  const [isButtonHovered, setIsButtonHovered] = useState(false); // Tracks if the button is hovered
   const timeoutRef = useRef(null);
+
   const resetTimeout = () => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
@@ -42,23 +43,28 @@ export const Slide = () => {
       resetTimeout();
     };
   }, [index]);
+
   return (
     <div>
-      <div class="gradient gradient--c"></div>
+      <div className="gradient gradient--c"></div>
       <div className="slideshow">
         <div
           className="slideshowSlider"
-          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+          style={{
+            transform: `translate3d(${-index * 100}%, 0, 0)`,
+          }}
         >
-          {images.map((item, index) => (
+          {images.map((item, idx) => (
             <div
-              className="slideHorse"
-              key={index}
-              style={{ backgroundImage: `url(${item})` }}
+              className={`slideHorse ${isButtonHovered ? "zoom" : ""}`}
+              key={idx}
+              style={{
+                backgroundImage: `url(${item})`,
+              }}
             />
           ))}
         </div>
-        { <div className="slideshowDots">
+        <div className="slideshowDots">
           {images.map((_, idx) => (
             <div
               key={idx}
@@ -68,11 +74,11 @@ export const Slide = () => {
               }}
             ></div>
           ))}
-        </div>}
+        </div>
         <div className="slideTools">
           <img
             src={isHoveredL ? SlideLBtnHo2 : SlideLBtn2}
-            alt="My"
+            alt="Previous"
             id="slideLBtn"
             onMouseEnter={() => setIsHoveredL(true)}
             onMouseLeave={() => setIsHoveredL(false)}
@@ -82,7 +88,7 @@ export const Slide = () => {
           />
           <img
             src={isHoveredR ? SlideRBtnHo2 : SlideRBtn2}
-            alt="My"
+            alt="Next"
             id="slideRBtn"
             onMouseEnter={() => setIsHoveredR(true)}
             onMouseLeave={() => setIsHoveredR(false)}
@@ -90,14 +96,14 @@ export const Slide = () => {
               index === images.length - 1 ? setIndex(0) : setIndex(index + 1);
             }}
           />
-          <img src={SlideL} alt="My" id="slideL" />
-          <img src={SlideR} alt="My" id="slideR" />
+          <img src={SlideL} alt="Left" id="slideL" />
+          <img src={SlideR} alt="Right" id="slideR" />
           <div id="lineL" style={{ zIndex: "50" }}></div>
           <div id="lineR"></div>
           <div className="fix">
             <div className="fixSlide">
               <div className="fixTop">
-              <div
+                <div
                   className="miniHorse1"
                   style={{
                     backgroundImage:
@@ -110,14 +116,6 @@ export const Slide = () => {
                         : `url(${fix})`,
                   }}
                 ></div>
-                { /*<div
-                  className="miniHorse2"
-                  style={
-                    index === 4
-                      ? { backgroundImage: `url(${Minihorse2})` }
-                      : { display: "none" }
-                  }
-                ></div> */}
               </div>
               <div>
                 <div className="fixMiddle">
@@ -125,7 +123,13 @@ export const Slide = () => {
                   do eiusmod tempor incididunt ut labore et dolore mmpor inc
                 </div>
                 <div className="fixBottom">
-                  <div className="fixBtn">PLAY NOW</div>
+                  <div
+                    className="fixBtn"
+                    onMouseEnter={() => setIsButtonHovered(true)} // Start zoom on hover
+                    onMouseLeave={() => setIsButtonHovered(false)} // Stop zoom on leave
+                  >
+                    PLAY NOW
+                  </div>
                 </div>
               </div>
             </div>
